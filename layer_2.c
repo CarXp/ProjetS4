a59a257fb8bbeee304f8f64e2d19ef0bfcf1b91f
RemakeTm <30905414+RemakeTm@users.noreply.github.com>
2019-02-26 20:35:53 +0100

    Add files via upload

--- layer_2.c
+++ layer_2.c
@@ -41,7 +41,7 @@ int parity_index(int stripe_id, virtual_disk_t disk)
 
 
 
-void write_stripe(stripe_t stripe, block_t parity,int pos, virtual_disk_t disk)
+void write_stripe(stripe_t stripe, int pos, virtual_disk_t disk)
 {
 	/* stripe contient deja le block de parité dans l'ordre, voir raid_define */
 	/* la stripe sera crée dans  write_chunk */
@@ -53,10 +53,57 @@ void write_stripe(stripe_t stripe, block_t parity,int pos, virtual_disk_t disk)
 
 
 void write_chunk(uchar buffer[],int noctet, virtual_disk_t disk){
-	//IL FAUDRA:
+
 	// 1 - Calculer le nombre de bande (compute nstripe)
+
+	int nblock = compute_nblock(noctet);
+	int nstripes = compute_nstripe(nblock);
+
 	// 2 - Créer un tableau pour chaque bande, écrire dedans puis créer le block de parité
-	// 3 - Mettre dans le bon odre des éléments du tableau (--> prévoir une case vide dans le tableau fait dans 2 pour y ajouter ensuite le block de parité) + compléter les fin de bandes (0 0 0 0) (à confirmer sujet)
+
+	stripe_t *tabstripe  = malloc(nstripes * sizeof(stripe_t));
+
+	int nbdisk = disk.ndisk;
+
+	int indicebuffer = 0;
+
+	block_t parity;
+
+	int index_parity;
+
+	for (int i = 0 ; i < nstripes ; i++)
+	{
+		tabstripe[i].nblocks = nbdisk;
+		tabstripe[i].stripe = malloc(nbdisk * sizeof(block_t));
+
+		index_parity = parity_index(i, disk);
+
+		
+		for (int j = 0 ; j < nbdisk ; j++)
+		{
+			if(indiceblock != index_parity)
+			{
+
+				int indiceblock = 0;
+
+				while(indicebuffer < noctet && indiceblock != )
+				{
+					tabstripe[i].stripe[j].data[indiceblock] = buffer[indicebuffer];
+					indicebuffer++;
+					indiceblock++;
+				}
+
+			}
+			
+		}
+
+		parity = compute_parity(nbdisk, tabstripe[i].stripe);
+
+		tabstripe[i].stripe[index_parity] = parity;
+		
+	}
+
+	// 3 - Mettre dans le bon odre les éléments du tableau (--> prévoir une case vide dans le tableau fait dans 2 pour y ajouter ensuite le block de parité) + compléter les fin de bandes (0 0 0 0) (à confirmer sujet)
 	// 4 - écrire dans le systeme avec write_stripe chaque bande 
 }
 
