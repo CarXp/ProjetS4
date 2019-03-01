feac363873197cb4895cedfe4aa7b63eed4ba63e
RemakeTm <30905414+RemakeTm@users.noreply.github.com>
2019-03-01 23:53:40 +0100

    Add files via upload

--- layer_2.c
+++ layer_2.c
@@ -37,7 +37,7 @@ block_t compute_parity(block_t * tabBlocks,int nblocks)
 
 int parity_index(int stripe_id, virtual_disk_t disk)
 {
-	return (disk.ndisk-(stripe_id%disk.ndisk)-1);
+	return (disk.ndisk - (stripe_id % disk.ndisk) - 1);
 }
 
 
@@ -53,14 +53,15 @@ void write_stripe(stripe_t stripe,int pos, virtual_disk_t disk)
 }
 
 
-void write_chunk(uchar buffer[],int noctet, virtual_disk_t disk, int startblock){
+void write_chunk(uchar buffer[],int noctet, virtual_disk_t disk, int startblock)
+{
 
 	// 1 - Calculer le nombre de bande (compute nstripe)
 
 	int nblock = compute_nblock(noctet);
 	//On calcule le nombre de bloc de parité qu'il faut rajouter
-	nblock+=nblock/(disk.ndisk-1)+1;
-	int nstripes = compute_nstripe(nblock,disk);
+	nblock += nblock / (disk.ndisk - 1) + 1;
+	int nstripes = compute_nstripe(nblock, disk);
 
 	// 2 - Créer un tableau pour chaque bande, écrire dedans puis créer le block de parité
 
@@ -71,38 +72,38 @@ void write_chunk(uchar buffer[],int noctet, virtual_disk_t disk, int startblock)
 	block_t parity;
 	int index_parity;
 
-	for (int i = 0 ; i < nstripes ; i++)
+	for (int i = 0 ; i < nstripes ; i++)	//Pour chaque bande
 	{	
+		int indiceblock = 0;
+
 		tabstripe[i].nblocks = nbdisk;
 		tabstripe[i].stripe = malloc(nbdisk * sizeof(block_t));
+
 		index_parity = parity_index(i, disk);
 
-		for(int a=0;a<BLOCK_SIZE;a++){
-			tabstripe[i].stripe[index_parity].data[a]=0;
-		}
+		for(int a = 0 ; a < BLOCK_SIZE ; a++)
+			tabstripe[i].stripe[index_parity].data[a] = 0;
 
-
-		for (int j = 0 ; j < nbdisk ; j++)	//Pour chaque bloc d'une bande
+		while(indicebuffer < noctet && indiceblock != tabstripe[i].nblocks)
 		{
-	
-			if(j != index_parity)
+			if (indiceblock != index_parity) 
 			{
-				int indiceblock=0;
-				while(indicebuffer < noctet && indiceblock != tabstripe[i].nblocks)	//Tant qu'on est pas à la fin du buffer et qu'on a pas dépasser la limite du bloc
-				{
-					tabstripe[i].stripe[j].data[indiceblock] = buffer[indicebuffer];
-					indicebuffer++;
-					indiceblock++;
-				}
-
+				tabstripe[i].stripe[indiceblock].data[indicebuffer % BLOCK_SIZE] = buffer[indicebuffer];
+				indicebuffer++;
+				
+				if (indicebuffer % BLOCK_SIZE == 0)
+					indiceblock++;		
 			}
-			
+
+			else
+				indiceblock++;
 		}
+		
+
 		// 3 - Mettre dans le bon odre les éléments du tableau (--> prévoir une case vide dans le tableau fait dans 2 pour y ajouter ensuite le block de parité) + compléter les fin de bandes (0 0 0 0) (à confirmer sujet)
 		parity = compute_parity(tabstripe[i].stripe,tabstripe[i].nblocks);
 
 		tabstripe[i].stripe[index_parity] = parity;
-		//for(int k=0;k<tabstripe[i].nblocks;k++) affichage_block(tabstripe[i].stripe[k]);
 
 		// 4 - écrire dans le systeme avec write_stripe chaque bande 
 		write_stripe(tabstripe[i], startblock, disk);
@@ -115,10 +116,13 @@ int read_stripe(stripe_t * stripe, int pos, virtual_disk_t disk)
 {
 	block_t inter;
 	stripe->nblocks = disk.ndisk;
-	for(int i = 0 ; i < disk.ndisk ; i++ ){
-		read_block(&inter,pos,i,disk);
+
+	for(int i = 0 ; i < disk.ndisk ; i++)
+	{
+		read_block(&inter, pos, i, disk);
 		stripe->stripe[i] = inter;
 	}
+
 	return SUCCESS;
 }
 
@@ -127,30 +131,35 @@ void read_chunk(int start_byte, uchar buffer[], int noctet, virtual_disk_t disk)
 {
 	int nblock = compute_nblock(noctet);
 	//On calcule le nombre de bloc de parité présents
-	nblock+=nblock/(disk.ndisk-1)+1;
-	int nstripes = compute_nstripe(nblock,disk);
+	nblock += nblock / (disk.ndisk - 1) + 1;
+	int nstripes = compute_nstripe(nblock, disk);
 
 	//start_byte est la position de départ
-	int pos=start_byte;
+	int pos = start_byte;
 	stripe_t stripeInter;
-	stripeInter.stripe=malloc(disk.ndisk*sizeof(block_t));
+	stripeInter.stripe = malloc(disk.ndisk * sizeof(block_t));
+
 	int indice_buffer = 0;
 	int indParity;
 	int idChar;
 
 	//Méthode: on détermine chaque stripe (dont le block de parité)
 	//Puis on lit tous les blocks dans le buffer, sauf les block de parité
-	for(int i = 0 ; i < nstripes ; i++){
+	for(int i = 0 ; i < nstripes ; i++)
+	{
 
-		read_stripe(&stripeInter,pos,disk);
-		indParity=parity_index(pos,disk);
+		read_stripe(&stripeInter, pos, disk);
+		indParity = parity_index(pos, disk);
 
 		//On traite désormais chaque bloc de la bande
-		for(int j = 0 ; j < stripeInter.nblocks ; j++){
-			idChar=0;
+		for(int j = 0 ; j < stripeInter.nblocks ; j++)
+		{
+			idChar = 0;
 			//On ne traite pas le block de parité
-			if(j != indParity){
-				while(idChar < 4 && indice_buffer < noctet) {
+			if(j != indParity)
+			{
+				while(idChar < BLOCK_SIZE && indice_buffer < noctet)
+				{
 					buffer[indice_buffer] = stripeInter.stripe[j].data[idChar];
 					indice_buffer++;
 					idChar++;
